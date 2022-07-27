683e5ac7dd7c912be2590fcd4311916722c682e8
Greg <greg19850@gmail.com>
2022-07-27 15:10:36 +0100

    changed code, created separate function to display input name with first letter uppercase. reated function checkLength, to check valid length of username & password

--- script.js
+++ script.js
@@ -3,12 +3,11 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-const showError = (input) => {
+const showError = (input, message) => {
   const formControlDiv = input.parentElement;
   formControlDiv.classList.add('error');
   const small = formControlDiv.querySelector('small');
-  const name = input.id[0].toUpperCase() + input.id.slice(1);
-  small.textContent = `${name} is required`;
+  small.textContent = message;
 };
 
 const showSuccess = (input) => {
@@ -16,17 +15,23 @@ const showSuccess = (input) => {
   formControlDiv.classList.add('success');
 };
 
+const displayName = (input) => input.id[0].toUpperCase() + input.id.slice(1);
+
 const checkIfValid = (inputArr) => {
   inputArr.forEach((input) => {
-    if (input.value.trim() === '') showError(input);
+    if (input.value.trim() === '')
+      showError(input, `${displayName(input)} is required`);
     else showSuccess(input);
   });
 };
+const checkLength = () => {};
 
 const submitForm = (e) => {
   e.preventDefault();
 
   checkIfValid([username, email, password, password2]);
+  checkLength(username, 5, 15);
+  checkLength(password, 6, 25);
 };
 
 document.getElementById('form').addEventListener('submit', submitForm);
