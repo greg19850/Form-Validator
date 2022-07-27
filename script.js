e1badce384049de1c6abb383995a26908fe8413d
Greg <greg19850@gmail.com>
2022-07-27 14:38:15 +0100

    added functions showError & showSuccess in checkIfValid function, which will control what will happen in case of incorrectly & correctly filled out input fields

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-const checkIfValid = () => {};
+const showError = () => {};
+
+const showSuccess = () => {};
+
+const checkIfValid = (inputArr) => {
+  inputArr.forEach((input) => {
+    if (input.value === '') showError();
+    else showSuccess();
+  });
+};
 
 const submitForm = (e) => {
   e.preventDefault();
