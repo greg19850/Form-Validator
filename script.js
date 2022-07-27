ab9181e1b311e311389c03bbf371e72f695bd8a2
Greg <greg19850@gmail.com>
2022-07-27 15:00:15 +0100

    updated functions showError & showSuccess - Added CSS classes changing fields colors, and error message in showError function

--- script.js
+++ script.js
@@ -3,14 +3,23 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-const showError = () => {};
+const showError = (input) => {
+  const formControlDiv = input.parentElement;
+  formControlDiv.classList.add('error');
+  const small = formControlDiv.querySelector('small');
+  const name = input.id[0].toUpperCase() + input.id.slice(1);
+  small.textContent = `${name} is required`;
+};
 
-const showSuccess = () => {};
+const showSuccess = (input) => {
+  const formControlDiv = input.parentElement;
+  formControlDiv.classList.add('success');
+};
 
 const checkIfValid = (inputArr) => {
   inputArr.forEach((input) => {
-    if (input.value === '') showError();
-    else showSuccess();
+    if (input.value.trim() === '') showError(input);
+    else showSuccess(input);
   });
 };
 
